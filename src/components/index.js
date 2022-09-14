880eef20b5690ff5e096ea39f98e2ba0acfb6639
andinianst93 <andinianst93@gmail.com>
2022-09-14 16:10:14 +0700

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Layout from './Layout'
+import Hero from './Hero'
+import About from './About'
+import Projects from './Projects'
+import Survey from './Survey'
+import Slider from './Slider'
+
+import GridProjects from './GridProjects'
+export { Layout, Hero, About, Projects, Survey, Slider, GridProjects }
